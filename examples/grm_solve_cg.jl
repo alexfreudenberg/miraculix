81208a85b3ffd8b46d7f3d875cd862d2c3e65b3a
Alexander Freudenberg <alexander.freudenberg@stads.de>
2023-05-19 14:26:06 +0200

    Add conjugate gradient + documentation

--- examples/grm_solve_cg.jl
+++ examples/grm_solve_cg.jl
@@ -16,10 +16,9 @@
 # The script will output the solution to the equation G^-1 x as a vector. 
 
 # # Exceptions
-# - Throws an error if the required module or its matrix multiplication function 
-#   cannot be found or loaded.
-# - Throws an error if the genomic relationship matrix G is not invertible.
-# - Throws an error if the data for G or x cannot be read or is incorrectly formatted.
+# - Throws an error if the path is wrong.
+# - Converges poorly if the genomic relationship matrix G is not invertible (e.g., snps > indiv).
+# - Throws an error if the PLINK bed file for G cannot be read or is incorrectly formatted.
 
 using Statistics
 using Libdl
@@ -33,10 +32,29 @@ FREQ_FILE = ROOT_DIR * "/data/xsmall.freq"
 
 include(MODULE_PATH)
 
+"""
+    GRM_vec(obj_ref::Ref{Ptr{Cvoid}}, B::Matrix{Float64}, snps::Int, indiv::Int)
+
+Multiplies the genomic relationship matrix (GRM) by a vector.
+
+# Arguments
+- `obj_ref`: A Ref{Ptr{Cvoid}} that holds a pointer to the storage object containing SNP data.
+- `B`: A one-column Matrix of Float64 representing the vector to be multiplied.
+- `snps`: An integer representing the number of SNPs.
+- `indiv`: An integer representing the number of individuals.
+
+This function first multiplies the transposed (centered) SNP matrix by the vector `B`, and then the result with the untransposed (centered) SNP matrix. This iterative process allows for efficient calculation of the GRM-vector multiplication.
+
+# Returns
+- The result of the GRM-vector multiplication.
+
+# Exceptions
+- Throws an error if the inputs are not in the expected format or if the multiplication operation fails.
+"""
 function GRM_vec(obj_ref::Ref{Ptr{Cvoid}}, B::Matrix{Float64}, snps::Int, indiv::Int)
 
-    if length(B) != indiv
-        error("Vector must be of length $indiv")
+    if size(B) != (indiv,1)
+        error("Vector B must be of length $indiv")
     end
     
     Zv = miraculix.dgemm_compressed.dgemm_compressed_main(true, obj_ref, B, n_snps, n_indiv)
@@ -45,41 +63,52 @@ function GRM_vec(obj_ref::Ref{Ptr{Cvoid}}, B::Matrix{Float64}, snps::Int, indiv:
     return Gv
 end
 
+# Set library path and load library
 miraculix.dgemm_compressed.set_library_path(LIBRARY_PATH)
 miraculix.dgemm_compressed.load_shared_library()
-miraculix.dgemm_compressed.set_options(use_gpu=false, verbose=0)
+# Set miraculix options
+miraculix.dgemm_compressed.set_options(use_gpu=!false, verbose=0)
 
+# Read PLINK and allele frequency files
 genotype_data, n_snps, n_indiv = miraculix.read_plink.read_bed(DATA_FILE)
 freq = miraculix.read_plink.read_freq(FREQ_FILE)
 
+# Initialize storage object
+obj_ref = miraculix.dgemm_compressed.init_compressed(genotype_data, n_snps, n_indiv, freq, 1)
 
-obj_ref = miraculix.dgemm_compressed.init_compressed(genotype_data, n_snps, n_indiv, freq, 10)
+# Set hyper parameters
+max_iter = 1_000 # Maximum number of iterations
+print_iter = 1e2 # Frequency of convergence information
+conv_crit = 1e-2 # Maximum norm of residual
 
-n_indiv = 10
-max_iter = 100
-A = diagm(randn(Float64,n_indiv).+10)
+# Simulate data for toy example to test for convergence of algorithm
+A = diagm(randn(Float64,n_indiv)./10 .+10) 
 A_vec(obj_ref, x, snps, indiv) = A * x
-begin 
-    b = randn(Float64, n_indiv, 1)
-    x = randn(Float64, n_indiv, 1)
 
-    Gx = A_vec(obj_ref, x, n_snps, n_indiv)
-    r = b - Gx
-    p = r
+# Start conjugate gradient 
+begin 
+    b = randn(Float64, n_indiv, 1) # RHS of equation system
+    x = randn(Float64, n_indiv, 1) # Start vector x0
+
+    Gx = GRM_vec(obj_ref, x, n_snps, n_indiv) # Calculate G * x0
+    r = b - Gx # Initial residual
+    p = r 
     for i in 1:max_iter
-        global x, r, p, Gp
+        global x, r, p, Gp, alpha, beta, norm_old
         norm_old = norm(r)
-        if norm_old < 1e-2
+        # Exit if convergence criterion is reached
+        if norm_old < conv_cirt
             break
         end
         
-        Gp = A_vec(obj_ref, p, n_snps, n_indiv)
-        alpha = norm_old / (transpose(p) * Gp)[1]
-        println(alpha, " ", norm_old)
+        Gp = GRM_vec(obj_ref, p, n_snps, n_indiv)
+        alpha = norm_old^2 / (transpose(p) * Gp)[1]
+        (i % print_iter == 0) && println(alpha, " ", norm_old)
 
         x += alpha * p
         r -= alpha * Gp
-        beta = norm(r)/norm_old
+        beta = norm(r)^2/norm_old^2
         p = r + beta * p
     end
+    println("Final residual norm: ", norm(r))
 end
