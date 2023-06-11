302c6d9318613b3a59582c74cb3b3a4187341901
Alexander Freudenberg <alexander.freudenberg@stads.de>
2023-06-11 11:02:56 +0200

    Harmonize naming conventions

--- src/cuda/mmagpu.cu
+++ src/cuda/mmagpu.cu
@@ -18,72 +18,94 @@ limitations under the License.
 
 // Include order is important!!!
 // Namespace conflicts if order is changed
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
 
 #include <inttypes.h>
-#include <thrust/device_vector.h>
-#include <thrust/device_malloc.h>
-#include <thrust/device_free.h>
-
-// CUTLASS includes
-#include <thrust/functional.h>
-#include <thrust/reduce.h>
-#include <thrust/system/cuda/memory.h>
-#include <thrust/iterator/transform_iterator.h>
-#include <thrust/iterator/discard_iterator.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <unistd.h>
 
 #include "cuda_utils.h"
 #include "mmagpuIntern.h"
 
-static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
-                               size_t individuals, double *ans,
-                               size_t TileSize) {
+const int default_tile_size = 2048;
 
+int gpuCrossprodIntern(unsigned int *CGM, size_t snps,
+                               size_t individuals, double *ans) {   
+    /*
+    xxx
+
+    */
+                              
     // Get number of threads
-    bool verbose = get_print_level() >= 0;
-    const char *omp_num_threads = getenv("OMP_NUM_THREADS");
-    int num_threads = 4;
-    if (omp_num_threads != NULL) {
-        num_threads = atoi(omp_num_threads);
-    }
-    if (verbose) {
-        char print_message[] = (omp_num_threads != NULL)
-                                ? ("OMP_NUM_THREADS is set")
-                                : ("OMP_NUM_THREADS is not set");
-        printf("%s, using %d threads.", print_message, num_threads);
-    }
-        //
-    const size_t BytesPerRow =
-        (1 + ((1 + (snps - 1) / CodesPerByte) - 1) / 32) * 32;
-    const size_t IntsPerRow = 1 + (BytesPerRow - 1) / sizeof(unsigned int);
-
-    // sanity checks
-    // limit Tilesize to individuals
-    TileSize = TileSize > individuals ? individuals : TileSize;
-
-    // calculates total memory requirements
-    size_t req_mem = 2 * BytesPerRow * TileSize * CodesPerByte +
-                    TileSize * TileSize * sizeof(unsigned int);
-    if (req_mem > free_mem)
-        ERR("Not enough memory available.");
-
+    cudaError_t err;
     // Input data
     cutlass::uint4b_t *d_Z_block1;
     cutlass::uint4b_t *d_Z_block2;
     // Buffer for output
-    int32_t *d_M;
-    // Buffer for copying back results from device to host
-    int32_t *h_M;
+    int32_t *d_M, *h_M;
 
-    const int size_of_input =
-        BytesPerRow * TileSize * CodesPerByte / MEMORY_FACTOR;
-    const int size_of_output = sizeof(int32_t) * TileSize * TileSize;
+    bool verbose = get_print_level() >= 0;
+    const char *env_num_threads = getenv("OMP_NUM_THREADS");
+    int num_threads = 4;
+    if (env_num_threads != NULL) {
+        num_threads = atoi(env_num_threads);
+    }
+    if (verbose) {
+        printf("Using %d OMP threads.\n", num_threads);
+    }
+
+    long n_bytes_per_snp =
+        (indiv - 1) / 4 + 1; // number of columns of Z if individuals
+                             // are zero-padded to be a multiple of 4
+    long n_indiv_per_byte = 8L / 2L;
+
+    // sanity checks
+    // limit Tilesize to individuals
+    const char *env_tile_size = getenv("TILE_SIZE");
+    int mem_tile_size = default_tile_size;
+    if (env_tile_size != NULL) {
+        mem_tile_size = atoi(env_tile_size);
+    }
+    debug_info("Using tile size of %d.\n", mem_tile_size);
+
+    mem_tile_size = min(individuals, mem_tile_size);
+
+    if (checkCuda() != 0) {
+        return 1;
+    }
+    int device = switchDevice();
+    if (device == -1) {
+        return 1;
+    }
+
+    // Calculate total memory requirements
+    size_t required_mem = num_threads * (2 * n_bytes_per_snp * mem_tile_size +
+                          mem_tile_size * mem_tile_size * sizeof(unsigned int));
+    if (checkDevMemory(required_mem) != 0) {
+        return 1;
+    }
+
+    int size_of_input = n_bytes_per_snp * mem_tile_size;
+    int size_of_output = sizeof(int32_t) * mem_tile_size * mem_tile_size;
     // Initialization of buffers: We calculate n_streams of tile matrix
     // multiplications in parallel and allocate the corresponding amount of memory
-    cudaMalloc((void **)&d_Z_block1, n_streams * size_of_input);
-    cudaMalloc((void **)&d_Z_block2, n_streams * size_of_input);
-    cudaMalloc((void **)&d_M, n_streams * size_of_output);
-    cudaMallocHost((void **)&h_M, n_streams * size_of_output);
-    err_check("Memory allocation: ");
+    err = cudaMalloc((void **)&d_Z_block1, n_streams * size_of_input);
+    if (checkError(__func__, __LINE__, err) != 0)
+        return (1);
+    err = cudaMalloc((void **)&d_Z_block2, n_streams * size_of_input);
+    if (checkError(__func__, __LINE__, err) != 0)
+        return (1);
+    err = cudaMalloc((void **)&d_M, n_streams * size_of_output);
+    if (checkError(__func__, __LINE__, err) != 0)
+        return (1);
+    err = cudaMallocHost((void **)&h_M, n_streams * size_of_output);
+    if (checkError(__func__, __LINE__, err) != 0)
+        return (1);
 
     // initialization of cutlass gemm operators
     using ColumnMajor = cutlass::layout::ColumnMajor;
@@ -129,7 +151,6 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
         OperatorClass_, ArchTag_, ElementA_, ElementB_, ElementC_,
         ElementAccumulator_>::Operator;
     const bool IsBetaZero = false;
-
     using CutlassGemm = cutlass::gemm::device::Gemm<
         ElementA_, LayoutA_, ElementB_, LayoutB_, ElementC_, LayoutC_,
         ElementAccumulator_, OperatorClass_, ArchTag_, ThreadblockShape_,
@@ -140,6 +161,9 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
     // Define a CUTLASS GEMM type
     CutlassGemm gemm_operator;
 
+    if (checkError(__func__, __LINE__, cudaGetLastError()) != 0)
+    return (1);
+
     // Main loop
     // Calculates matrix multiplications in parallel: Each thread in this loop sends
     // its data to a different stream on the device. The threads calculate
@@ -148,43 +172,40 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
     #ifdef DO_PARALLEL
     #pragma omp parallel for num_threads(num_threads) schedule(dynamic)
     #endif
-    for (int64_t i = 0; i < individuals; i += TileSize) {
+    for (int64_t i = 0; i < individuals; i += mem_tile_size) {
         int threadidx = omp_get_thread_num();
         cudaStream_t stream;
-        cudaError_t err = cudaStreamCreate(&stream);
+        
+         err = cudaStreamCreate(&stream);
         cudaStreamSynchronize(stream);
-
-        if (err != cudaSuccess)
-        ERR("Stream couldn't be created");
 
         // Pointer to the first element of current rows
         unsigned int *x = (CGM + i * IntsPerRow);
         cutlass::uint4b_t *d_tile1 =
-            d_Z_block1 + threadidx * TileSize * BytesPerRow;
+            d_Z_block1 + threadidx * mem_tile_size * n_bytes_per_snp;
         cutlass::uint4b_t *d_tile2 =
-            d_Z_block2 + threadidx * TileSize * BytesPerRow;
+            d_Z_block2 + threadidx * mem_tile_size * n_bytes_per_snp;
 
         // Number of rows in matrix
         size_t const rows_left = individuals - i;
         // Size x of current tile
-        size_t const x_tile_size = TileSize < rows_left ? TileSize : rows_left;
+        size_t const x_tile_size = min(mem_tile_size, rows_left);
 
-        cudaMemcpyAsync(d_tile1, x, x_tile_size * BytesPerRow,
+        cudaMemcpyAsync(d_tile1, x, x_tile_size * n_bytes_per_snp,
                         cudaMemcpyHostToDevice, stream);
 
         cudaStreamSynchronize(stream);
         err_check("Copy 1:");
 
         // Inner loop
-        for (int64_t j = i; j < individuals; j += TileSize) {
+        for (int64_t j = i; j < individuals; j += mem_tile_size) {
 
         // Same as above with y
         size_t const columns_left = individuals - j;
-        size_t const y_tile_size =
-            TileSize < columns_left ? TileSize : columns_left;
+        size_t const y_tile_size = min(mem_tile_size, columns_left);
         unsigned int *y = (CGM + j * IntsPerRow);
 
-        cudaMemcpyAsync(d_tile2, y, y_tile_size * BytesPerRow,
+        cudaMemcpyAsync(d_tile2, y, y_tile_size * n_bytes_per_snp,
                         cudaMemcpyHostToDevice, stream);
         err_check("Copy 2:");
         cudaStreamSynchronize(stream);
@@ -192,11 +213,11 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
         // initialize gemm arguments
         CutlassGemm::Arguments args(
             {int(x_tile_size), int(y_tile_size),
-            int(BytesPerRow * CodesPerByte / COMPRESSION_GPU)},
-            {d_tile1, int(BytesPerRow * CodesPerByte / COMPRESSION_GPU)},
-            {d_tile2, int(BytesPerRow * CodesPerByte / COMPRESSION_GPU)},
-            {d_M + threadidx * TileSize * TileSize, int(y_tile_size)},
-            {d_M + threadidx * TileSize * TileSize, int(y_tile_size)}, {1, 0});
+            int(n_bytes_per_snp * n_indiv_per_byte)},
+            {d_tile1, int(n_bytes_per_snp * n_indiv_per_byte)},
+            {d_tile2, int(n_bytes_per_snp * n_indiv_per_byte)},
+            {d_M + threadidx * mem_tile_size * mem_tile_size, int(y_tile_size)},
+            {d_M + threadidx * mem_tile_size * mem_tile_size, int(y_tile_size)}, {1, 0});
         cudaStreamSynchronize(stream);
 
         // compute Multiplication
@@ -208,9 +229,9 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
         err_check("Calculation:");
 
         // Copy results back to host
-        cudaMemcpyAsync(h_M + threadidx * TileSize * TileSize,
-                        d_M + threadidx * TileSize * TileSize,
-                        TileSize * TileSize * sizeof(int32_t),
+        cudaMemcpyAsync(h_M + threadidx * mem_tile_size * mem_tile_size,
+                        d_M + threadidx * mem_tile_size * mem_tile_size,
+                        mem_tile_size * mem_tile_size * sizeof(int32_t),
                         cudaMemcpyDeviceToHost, stream);
         err_check("Copying back:");
 
@@ -226,7 +247,7 @@ static void gpuCrossprodIntern(unsigned int *CGM, size_t snps,
         for (int64_t di = 0; di < x_tile_size; ++di) {
             for (int64_t dj = 0; dj < y_tile_size; ++dj) {
             // Get result
-            int32_t *Mij  = *(h_M + threadidx * TileSize * TileSize + dj + di * y_tile_size);
+            int32_t *Mij  = *(h_M + threadidx * mem_tile_size * mem_tile_size + dj + di * y_tile_size);
             double *ans0 = ans + (i + di),
                    *ans1 = ans + (i + di) * individuals;
             ans0[(j + dj) * individuals] = (double)Mij;
